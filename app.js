af0a11ee085470c8620a44103b688b97d43d0ea5
Marcus Reis <marcus.reis@askblue.pt>
2023-09-06 20:46:52 +0100

    Mongoose Setup & Driver Implementation

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./routes/routes')
 const app = express()
+
+require('dotenv').config()
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGO_URI)
 
 app.use(bodyParser.json())
 routes(app)
